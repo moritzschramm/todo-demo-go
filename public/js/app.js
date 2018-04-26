9cd46a92b85f89d8f84dadb4f166b6e597cda13a
Moritz Schramm <moritz.schramm1@gmail.com>
2018-04-26 19:55:53 +0200

    implement transitions when list gets updated, add sorting functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,12 @@
-
-
 var app = new Vue({
   el: '#app',
   data: {
     todos: [],
-    text: ""
+    text: "",
+    maxId: 99,
+    loading: true,
+    orderSelection: "created_at",
+    noItemsText: ""
   },
   created: function() {
 
@@ -15,17 +17,44 @@ var app = new Vue({
 			
 			var todos = JSON.parse(this.responseText);
 			todos.forEach(function(todo) {
-				vm.todos.push({id: todo.id, note: todo.note, done: todo.done, inEdit: false});
+				vm.todos.push({id: todo.id, note: todo.note, done: todo.done, created_at: todo.created_at, updated_at: todo.updated_at});
+				if(todo.id >= vm.maxId) {
+					vm.maxId = todo.id + 1;
+				}
 			});
+			vm.loading = false;
 		}
 	}.bind(xhttp, this);
 	xhttp.send();
   },
+  mounted: function() {
+
+	document.getElementById('note-input').focus();
+  },
+  watch: {
+  	orderSelection: function(order) {
+  		
+  		this.sortNotes();
+  		document.getElementById('note-input').focus();
+  	}
+  },
   methods: {
+  	sortNotes: function() {
+
+  		var order = this.orderSelection;
+  		this.todos.sort(function(a, b) {
+  			if(a[order] < b[order]) {
+  				return -1;
+  			} else if (a[order] > b[order]) {
+  				return 1;
+  			}
+  			return 0;
+  		});
+  	},
   	addNote: function() {
   		if(this.text !== "") {
-	  		this.todos.push({id: -1, note: this.text, done: false, inEdit: false});
-	  		this.text = "";
+
+  			this.loading = true;
 
 	  		var xhttp = new XMLHttpRequest();
 	  		xhttp.open("POST", "/todo", true);
@@ -34,15 +63,21 @@ var app = new Vue({
 			if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
 					
 					var todo = JSON.parse(this.responseText);
-					vm.todos[vm.todos.length-1].id = todo.id;
+					vm.todos.push({id: todo.id, note: todo.note, done: todo.done, created_at: todo.created_at, updated_at: todo.updated_at});
+			  		vm.text = "";
+			  		vm.sortNotes();
+					vm.to
+					if(todo.id >= vm.maxId) {
+						vm.maxId = todo.id + 1;
+					}
+					vm.loading = false;
 				}
 			}.bind(xhttp, this);
-	  		xhttp.send(encodeTodo(this.todos[this.todos.length-1]));
+	  		xhttp.send(encodeTodo({note: this.text, done: false}));
   		}
+  		document.getElementById('note-input').focus();
   	},
   	editNote: function(todo) {
-
-  		todo.inEdit = false;
 
   		this.editTodo(todo);
   	},
@@ -58,6 +93,7 @@ var app = new Vue({
 	  	xhttp.open("POST", "/edit/todo/"+todo.id, true);
 	  	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	  	xhttp.send(encodeTodo(todo));
+	  	this.sortNotes();
   	},
   	deleteNote: function(todo, index) {
   		this.todos.splice(index, 1);
@@ -65,9 +101,10 @@ var app = new Vue({
   		var xhttp = new XMLHttpRequest();
 	  	xhttp.open("POST", "/delete/todo/"+todo.id, true);
 	  	xhttp.send();
+	  	document.getElementById('note-input').focus();
   	}
   }
-})
+});
 
 function encodeTodo(todo) {
 
