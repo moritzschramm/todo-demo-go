6c1e07e248a4fd1079fd6fb070f873eb984fffaa
Moritz Schramm <moritz.schramm1@gmail.com>
2018-05-01 15:40:48 +0200

    add negroni

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 go get -v github.com/go-sql-driver/mysql
 go get -v github.com/julienschmidt/httprouter
-go run main.go
+go get -v github.com/urfave/negroni
+go install github.com/moritzschramm/todo-demo-go
+$GOPATH/bin/todo-demo-go
